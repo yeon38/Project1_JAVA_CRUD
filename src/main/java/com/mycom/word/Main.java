03157fada6d190c92e683a62ca4d16cb9629728d
yeon38 <chaeyeon6243@handong.ac.kr>
2023-09-05 16:53:33 +0900

    first commit 9/5

---
+++ src/main/java/com/mycom/word/Main.java
@@ -0,0 +1,11 @@
+package com.mycom.word;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello world 안녕");
+        new WordManager().Start();
+    }
+
+}
