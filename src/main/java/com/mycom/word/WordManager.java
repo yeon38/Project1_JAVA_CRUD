d7b8280d01b5aa4ffe5cb2a3ce5061dff17e331c
yeon38 <chaeyeon6243@handong.ac.kr>
2023-09-08 12:14:10 +0900

    [Feat][docs]

--- src/main/java/com/mycom/word/WordManager.java
+++ src/main/java/com/mycom/word/WordManager.java
@@ -3,20 +3,7 @@ import java.util.Scanner;
 public class WordManager {
     Scanner s = new Scanner(System.in);
     WordCRUD wordCRUD;
-    /*
-     ***영단어 마스터 ***
-     *******************
-     1. 모든 단어 보기
-     2. 수준별 단어 보기
-     3. 단어 검색
-     4. 단어 추가
-     5. 단어 수정
-     6. 단어 삭제
-     7. 파일 저장
-     0. 나가기
-     ********************
-     => 원하는 메뉴는?
-     */
+
     WordManager() {
         wordCRUD = new WordCRUD(s);
     }
